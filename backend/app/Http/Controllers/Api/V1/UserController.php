3af784576338137a9488bf7322f1cdafd02b5b60
hideyuki <hidesnow0113@gmail.com>
2026-08-16 13:02:31 +0900

    Sanctumを使用した認証APIを実装

---
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(Request $request): UserResource
+    {
+        return new UserResource($request->user());
+    }
+}
